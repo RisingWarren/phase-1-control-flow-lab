3d3b39ebfb4ef6b63b5ec50ae58d807f41f6d902
Warren Santiago <wsantiago9623@gmail.com>
2022-09-20 16:08:07 -0400

    commit tree 737802b0ca5f5fd5da82fb0e1e3201d62e1c52ed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(string){
   // Write your code here!
+
+  if (string <= 400) return "This one is on me!";
+ if (string < 2000) return "That will be twenty bucks.";
+ if (string > 2500) return "No can do.";
+ if (string > 2000) return "I will gladly take your thirty bucks."
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(str){
   // Write your code here!
+  return str == 'NYC'?'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tam){
+  // Write your code here! 
+switch (tam){
+  case 'generous':
+    return "Thank you so much."
+  case 'not as generous':
+    return 'Thank you.'
+  default:
+    return "Bye."
+
+
+}
+
 }
